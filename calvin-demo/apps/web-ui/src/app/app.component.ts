5dea56a53e817ff85f464f1587b79043a1bd49c0
Calvin Zhu <calvinzhu@outlook.com>
2023-09-29 16:28:13 -0600

    Added material lib

--- calvin-demo/apps/web-ui/src/app/app.component.ts
+++ calvin-demo/apps/web-ui/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MaterialModule } from '@libs/common/ui/material';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent,RouterModule, CommonModule, MaterialModule],
   selector: 'calvin-demo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
--- calvin-demo/apps/web-ui/src/app/app.config.ts
+++ calvin-demo/apps/web-ui/src/app/app.config.ts
@@ -1,10 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { MaterialModule } from '@libs/common/ui/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+
+  providers: [
+    provideAnimations(),
+    provideHttpClient(),
+    importProvidersFrom(CommonModule, BrowserAnimationsModule, MaterialModule, MatNativeDateModule),
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation())
+  ],
 };
