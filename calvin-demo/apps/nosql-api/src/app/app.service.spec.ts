83710e36b85df39956f1e6d495affe0ae2b67f63
Calvin Zhu <calvinzhu@outlook.com>
2023-10-10 14:05:36 -0600

    Fixed an issue in tests

--- calvin-demo/apps/nosql-api/src/app/app.service.spec.ts
+++ calvin-demo/apps/nosql-api/src/app/app.service.spec.ts
@@ -1,16 +1,19 @@
 import { Test } from '@nestjs/testing';
 
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AppService', () => {
   let service: AppService;
+  let configService: ConfigService
 
   beforeAll(async () => {
     const app = await Test.createTestingModule({
-      providers: [AppService],
+      providers: [AppService, ConfigService],
     }).compile();
 
     service = app.get<AppService>(AppService);
+    configService = app.get<ConfigService>(ConfigService);
   });
 
   describe('getData', () => {
